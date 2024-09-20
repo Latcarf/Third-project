e0b7c01bd186fa3b3ed1388639ed597d6792c6ea
Latcarf <topmail2222@gmail.com>
2024-09-20 22:32:02 +0300

    Fix some mistake

--- src/main/java/KinoCMS/TestProjectApplication.java
+++ src/main/java/KinoCMS/TestProjectApplication.java
@@ -1,23 +1,12 @@
 package KinoCMS;
 
-<<<<<<< HEAD
-
-=======
->>>>>>> parent of 2bc9889 (Revert "Start")
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TestProjectApplication {
 
-<<<<<<< HEAD
     public static void main(String[] args) {
         SpringApplication.run(TestProjectApplication.class, args);
     }
 }
-=======
-	public static void main(String[] args) {
-		SpringApplication.run(TestProjectApplication.class, args);
-	}
-}
->>>>>>> parent of 2bc9889 (Revert "Start")
--- src/main/java/KinoCMS/config/SecurityConfig.java
+++ src/main/java/KinoCMS/config/SecurityConfig.java
@@ -2,7 +2,6 @@ package KinoCMS.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-<<<<<<< HEAD
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -23,35 +22,4 @@ public class SecurityConfig {
 
         return http.build();
     }
-=======
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
-
-@Configuration
-@EnableWebSecurity
-public class SecurityConfig {
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests(authorize -> authorize
-                        .anyRequest().permitAll()
-                )
-                .oauth2Login(oauth2Login -> oauth2Login
-                        .defaultSuccessUrl("/admin/main/requests", true)
-                )
-                .csrf(csrf -> csrf.disable());
-        return http.build();
-    }
-
-
->>>>>>> parent of 2bc9889 (Revert "Start")
 }
